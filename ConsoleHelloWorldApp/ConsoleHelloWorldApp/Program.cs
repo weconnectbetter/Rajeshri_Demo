8334ec240a88e3098b7c6412407d7651ad49bf7b
rajeshri <rajeshri@powersmpp.com>
2023-04-04 16:38:42 +0530

    commit

---
+++ ConsoleHelloWorldApp/ConsoleHelloWorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleHelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
